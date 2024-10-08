14cb4b648df57b159b3fb632982fe0db988b433a
hugolealtech <76668742+hugolealtech@users.noreply.github.com>
2024-10-08 17:41:17 -0300

    Home Page terminada
    Home Page terminada. Falta implementar Javascript na construção dos cards

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+class Cartao {
+
+    constructor (srcImg,titulo, subtitulo, descricao ){
+
+        this.srcImg = srcImg;
+        this.titulo = titulo;
+        this.subtitulo = subtitulo;
+        this.descricao = descricao;
+    }
+}
+
+var todosCartoes = [];
+
+todosCartoes.push(new Cartao (""));
+
+todosCartoes.forEach(cadaCartao) =>{
+
+    document.querySelector(#secao3)
+}
