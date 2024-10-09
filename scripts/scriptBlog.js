eb0a7c5c6a0be256c7e966df378cc57265d5fc4f
hugolealtech <76668742+hugolealtech@users.noreply.github.com>
2024-10-09 11:58:22 -0300

    Ainda tentando ajustar os cards lado a lado!

--- scripts/scriptBlog.js
+++ scripts/scriptBlog.js
@@ -21,28 +21,40 @@ todosCartoes.push(new Cartao(
 ));
 
 todosCartoes.push(new Cartao(
-    "/img/original/fotos/ben-guerin-NWrye3NRrKw-unsplash.jpg",
-    "CACHOEIRA DO TÉRMINO DE RELACIONAMENTO", 
-    "Dizem que banhar-se aqui, te traz o amor!", 
-    "Exuberante, tanto pela cultura, quanto pela beleza ímpar. É um privilégio da natureza este lugar!",
-    "/img/original/time/elizabeth.jpg", 
-    "Elisabeth"
+    "/img/original/fotos/boxed-water-is-better-LxaorEDmI3c-unsplash.jpg",
+    "RIO DA FELICIDADE", 
+    "Um lugar de paz e tranquilidade", 
+    "Aqui, o criador sussura palavras de sabedoria por meio do som das águas",
+    "/img/original/time/alice.jpg", 
+    "Alice"
 ));
 
 todosCartoes.push(new Cartao(
-    "/img/original/fotos/blake-verdoorn-cssvEZacHvQ-unsplash.jpg",
-    "CACHOEIRA DA RENOVAÇÃO", 
-    "Banhar-me aqui garantiu longevidade para minha vida amorosa!", 
-    "Exuberante, seja para visitar, seja para meditar ou banhar-se. Essas águas brotam do trono do próprio Deus!",
-    "/img/original/time/sergio.jpg", 
-    "Sérgio"
+    "/img/original/fotos/boxed-water-is-better-LxaorEDmI3c-unsplash.jpg",
+    "RIO DA FELICIDADE", 
+    "Um lugar de paz e tranquilidade", 
+    "Aqui, o criador sussura palavras de sabedoria por meio do som das águas",
+    "/img/original/time/alice.jpg", 
+    "Alice"
 ));
 
 
-todosCartoes.forEach((cadaCartao) => {
-    document.querySelector("#tituloSite3").innerHTML += `
+todosCartoes.push(new Cartao(
+    "/img/original/fotos/boxed-water-is-better-LxaorEDmI3c-unsplash.jpg",
+    "RIO DA FELICIDADE", 
+    "Um lugar de paz e tranquilidade", 
+    "Aqui, o criador sussura palavras de sabedoria por meio do som das águas",
+    "/img/original/time/alice.jpg", 
+    "Alice"
+));
 
-        <div id="tituloSite3">
+
+
+
+todosCartoes.forEach((cadaCartao) => {
+    document.querySelector(".tituloSite3").innerHTML += `
+
+        <div class="tituloSite3">
             <div id="cardsNamePlace">
                 <img class="img-thumbnail" class="w-50" class="card-img" src="${cadaCartao.srcImg}" alt="">
                 <div class="cardDescription">
@@ -58,3 +70,4 @@ todosCartoes.forEach((cadaCartao) => {
         </div>
     `;
 });
+
